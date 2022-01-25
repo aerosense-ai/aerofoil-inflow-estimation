69773f7b59889c2c37ee17fca660b6a15bdd78ee
Juri <yuriy.marykovskiy@mail.polimi.it>
2022-01-26 00:09:08 +0100

    ENH: Introduce flow estimation

--- aiem/aerofoil_inflow_models.py
+++ aiem/aerofoil_inflow_models.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.optimize import least_squares
 
 
-def estimate_from_potential_flow(pressure, eta, i0):
+def estimate_stagnation_from_potential_flow(pressure, eta, i0):
     """Estimates the location of the stagnation point.
     
-    :param eta: sensor locations in non-dimensional curvilinear coordinates (eta)
+    :param eta: sensor locations in non-dimensional conformal plane coordinates (eta)
     :param pressure: pressure at sensor locations.
     :param i0: null sensor index in the pressure vector
     :return: Stagnation point location in curvilinear coordinate
@@ -24,7 +24,7 @@ def estimate_from_potential_flow(pressure, eta, i0):
         sigma0 = 0
         bounds = (-0.5, 0.5)
 
-    popt = least_squares(potential_flow_cost_function, sigma0,
+    popt = least_squares(_potential_flow_cost_function, sigma0,
                          args=(eta, pressure, i0),
                          bounds=bounds,
                          verbose=1)
@@ -32,7 +32,18 @@ def estimate_from_potential_flow(pressure, eta, i0):
     return popt.x[0]
 
 
-def potential_flow_cost_function(sigma_p, xdata, ydata, i0):
+def estimate_speed_from_potential_flow(eta_stagnation, pressure, eta, i0, rho):
+    c = np.matmul(1/np.delete(_potential_flow_function(eta_stagnation, eta, i0), i0), np.delete(np.array(pressure), i0))
+    flow_speed = np.sqrt(2*c/rho)
+    return flow_speed
+
+
+def _potential_flow_function(sigma_p, xdata, i0):
+    a = (xdata[i0] - sigma_p) ** 2 / (1 + xdata[i0] ** 2) - (xdata - sigma_p) ** 2 / (1 + xdata ** 2)
+    return a
+
+
+def _potential_flow_cost_function(sigma_p, xdata, ydata, i0):
     """Cost function to perform non-linear least square optimisation
     c = 0.5*rho*U^2
     y_est = A*c
@@ -43,7 +54,7 @@ def potential_flow_cost_function(sigma_p, xdata, ydata, i0):
     :param i0: null sensor index in the pressure vector
     :return: cost to minimise
     """
-    A = (xdata[i0] - sigma_p) ** 2 / (1 + xdata[i0] ** 2) - (xdata - sigma_p) ** 2 / (1 + xdata ** 2)
+    A = _potential_flow_function(sigma_p, xdata, i0)
     c = np.linalg.lstsq(np.atleast_2d(A).T, ydata)  # Least square solution for linear parameters c
     y_est =A*c[0]
 
